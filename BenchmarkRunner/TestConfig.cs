06c5e84affbb0bbd885eac2739b7738101ce7ed0
Anton Firszov <anton.firszov@sapagroup.com>
2017-06-03 19:34:38 +0200

    config

--- BenchmarkRunner/TestConfig.cs
+++ BenchmarkRunner/TestConfig.cs
@@ -2,14 +2,18 @@ namespace BenchmarkRunner
 {
     using BenchmarkDotNet.Configs;
     using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Environments;
     using BenchmarkDotNet.Jobs;
 
     class TestConfig : ManualConfig
     {
         public TestConfig()
         {
-            this.Add(Job.Core);
-            this.Add(Job.Clr);
+            var coreJob = Job.Dry.With(Runtime.Core).WithId("Dry.Core");
+            var clrJob = Job.Dry.With(Runtime.Clr).WithId("Dry.Clr");
+
+            this.Add(coreJob);
+            this.Add(clrJob);
             this.Add(MemoryDiagnoser.Default);
         }
     }
