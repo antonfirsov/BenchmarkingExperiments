2b65e2b043e2150adf62948c404d163f99fd18a8
Anton Firszov <anton.firszov@sapagroup.com>
2017-06-03 19:59:01 +0200

    System.Drawing benchmarx!

---
+++ BenchmarkRunner/TestCoreCompatDrawing.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Drawing;
+
+namespace BenchmarkRunner
+{
+    [Config(typeof(TestConfig))]
+    public class TestCoreCompatDrawing
+    {
+        [Benchmark]
+        public void CreateBitmap()
+        {
+            using (var bitmap = new Bitmap(10, 10))
+            {
+                bitmap.SetPixel(0, 0, Color.Red);
+            }
+        }
+
+    }
+}
